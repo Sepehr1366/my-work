1de1ee169ac0d9e0c59472b19e1cd9f81a57c024
Sepehr1366 <sepehrnaji1987@gmail.com>
2018-05-17 21:53:35 -0700

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+
+    let Wrapp = document.getElementById('Wrapper');
+    let toplayer = document.querySelector('.Top');
+    let hand = document.querySelector('.Handel_it');
+    let sk= 0;
+    let delta = 0;
+    if (Wrapp.className.indexOf('skew') != -1) {
+
+    sk = 999;
+
+    }
+    Wrapp.addEventListener('mousemove', function (e) {
+
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+        hand.style.left = e.clientX + delta + 'px';
+        toplayer.style.width = e.clientX + sk + delta + 'px';
+    })
+})
